e852efd4f061c5abfa31151e0a9458cec1f90881
Tim Shannon <tim@townsourced.com>
2017-11-02 18:24:37 -0500

    Trying docker-compose for CI
    It's difficult and against common docker rules of them to run something
    like a database, then run your tests in the same container.
    
    The recommended route is to use multiple containers, which means
    compose.  Compose however makes it difficult to get proper exit codes
    from one or more test suites, and it doesn't have as nice integration
    with Jenkins.  Jenkins always assumes a single container for all of it's
    steps.

--- ci/testDB.sh
+++ ci/testDB.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-name=lexlibrary$1
-
-docker build -t $name ./$1
-
-docker run -v $PWD/..:/go/src/github.com/lexLibrary/lexLibrary $name sh -c '
-    cd /go/src/github.com/lexLibrary/lexLibrary/ci
-    sh test.sh
-'
+cd $1
+docker-compose run --rm tests --force-recreate
