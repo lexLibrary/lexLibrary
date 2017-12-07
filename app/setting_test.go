f8507b9525f6886db69f5d4a8c0c701a99d621d2
Tim Shannon <tim@townsourced.com>
2017-12-06 18:08:23 -0600

    Added another settings test

--- app/setting_test.go
+++ app/setting_test.go
@@ -3,17 +3,22 @@
 package app_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/lexLibrary/lexLibrary/app"
 	"github.com/lexLibrary/lexLibrary/data"
 )
 
-func TestSetting(t *testing.T) {
+func settingReset(t *testing.T) {
 	_, err := data.NewQuery("delete from settings").Exec()
 	if err != nil {
 		t.Fatalf("Error emptying settings table before running tests: %s", err)
 	}
+}
+
+func TestSetting(t *testing.T) {
+	settingReset(t)
 
 	t.Run("Default", func(t *testing.T) {
 		setting, err := app.SettingDefault("AllowPublic")
@@ -152,6 +157,78 @@ func TestSetting(t *testing.T) {
 			t.Fatalf("Bad value wasn't updated to default value. Expected %v, got %v", d.Value, s.Value)
 		}
 
+	})
+
+	t.Run("Settings List", func(t *testing.T) {
+		settingReset(t)
+
+		settings, err := app.Settings()
+		if err != nil {
+			t.Fatalf("Couldn't get list of all settings: %s", err)
+		}
+
+		for i := range settings {
+			d, err := app.SettingDefault(settings[i].Key)
+			if err != nil {
+				t.Fatalf("Error getting default setting for %s: %s", settings[i].Key, err)
+			}
+
+			if !reflect.DeepEqual(d.Value, settings[i].Value) {
+				t.Fatalf("Unset setting value was not defaulted.  Expected %v, Got %v", d.Value, settings[i].Value)
+			}
+		}
+
+		key := "AllowPublic"
+		d, err := app.SettingDefault(key)
+		if err != nil {
+			t.Fatalf("Error getting default setting for %s: %s", key, err)
+		}
+
+		app.SettingSet(key, !d.Bool())
+
+		settings, err = app.Settings()
+		if err != nil {
+			t.Fatalf("Couldn't get list of all settings: %s", err)
+		}
+
+		for i := range settings {
+			if settings[i].Key == key {
+				if settings[i].Bool() == d.Bool() {
+					t.Fatalf("Settings value didn't update. Expected %v got %v", d.Bool(), settings[i].Bool())
+					break
+				}
+			}
+		}
+
+	})
+
+	t.Run("Must", func(t *testing.T) {
+		settingReset(t)
+		key := "AllowPublic"
+		s := app.SettingMust(key)
+		if s.Key != key {
+			t.Fatalf("Invalid Key returned. Expected %s got %s", key, s.Key)
+		}
+
+		d, err := app.SettingDefault(key)
+		if err != nil {
+			t.Fatalf("Error getting setting default: %s", err)
+		}
+
+		if d.Bool() != s.Bool() {
+			t.Fatalf("Must did not return the default setting for an unset setting.  Expected %v, Got %v",
+				d.Value, s.Value)
+		}
+	})
+
+	t.Run("Must with Invalid key", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Fatalf("SettingMust did not panic with a bad key")
+			}
+		}()
+
+		_ = app.SettingMust("badKey")
 	})
 
 }
