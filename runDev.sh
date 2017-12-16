f4c350213f23e42b61f3f857466b542d07bade49
Tim Shannon <tim@townsourced.com>
2017-12-15 19:02:13 -0600

    Work on runDev.sh command.

--- runDev.sh
+++ runDev.sh
@@ -2,23 +2,29 @@
 
 set -e
 
+mkdir -p client/deploy
+
+VERSION=$(git describe --tags --long)
+
+# set version and git sha in version file
+echo "$VERSION">version
+
 go-bindata -debug -nomemcopy -prefix $PWD/client/deploy -pkg files -o files/bindata.go ./version ./client/deploy/...
+
 go build -o lexLibrary
 
 
-trap 'kill %1; kill %2' SIGINT
+cd client 
+gulp watch | sed -e 's/^/[Gulp] /' &
+gpid=$!
 
-killgroup(){
-  echo killing...
-  kill 0
-}
+cd ..
+ 
+./lexLibrary | sed -e 's/^/[LexLibrary] /' &
 
-# loop(){
-#   echo $1
-#   sleep $1
-#   loop $1
-# }
+lpid=$!
 
-# loop 1 &
-# loop 2 &
-# wait
+
+trap "kill ${lpid}; kill ${gpid}" SIGINT
+
+wait
