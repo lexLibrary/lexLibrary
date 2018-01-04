c0ed005a3a97b05174bd9dc7f65762f30bfb00c2
Tim Shannon <tim@townsourced.com>
2018-01-04 16:56:56 -0600

    More session work
    * CSRF updates
    * logging out
    * Getting users from sessions

--- app/session.go
+++ app/session.go
@@ -48,6 +48,8 @@ var (
 		{{arg "created"}},
 		{{arg "updated"}}
 	)`)
+	sqlSessionSetValid = data.NewQuery(`update sessions set valid = {{arg "valid"}} where id = {{arg "id"}}`)
+	sqlSessionSetCSRF  = data.NewQuery(`update sessions set csrf_token = {{arg "csrf_token"}} where id = {{arg "id"}}`)
 )
 
 // SessionNew generates a new session for the passed in user
@@ -89,5 +91,47 @@ func (s *Session) insert() error {
 		sql.Named("created", s.Created),
 		sql.Named("updated", s.Updated),
 	)
+	return err
+}
+
+// Logout logs a session out
+func (s *Session) Logout() error {
+	s.Valid = false
+	_, err := sqlSessionSetValid.Exec(sql.Named("valid", s.Valid), sql.Named("id", s.ID))
+	return err
+}
+
+// User returns the user for the given login session
+func (s *Session) User() (*User, error) {
+	u := &User{}
+	err := sqlUserFromID.QueryRow(sql.Named("id", s.UserID)).Scan(
+		&u.ID,
+		&u.Username,
+		&u.FirstName,
+		&u.LastName,
+		&u.AuthType,
+		&u.Active,
+		&u.Version,
+		&u.Updated,
+		&u.Created)
+	if err == sql.ErrNoRows {
+		return nil, ErrSessionInvalid
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	if !u.Active {
+		return nil, ErrSessionInvalid
+	}
+
+	return u, nil
+}
+
+// ResetCSRF will generate a new CRSF token, an update the session with it
+// allows CSRF token to change more than once per session if need be
+func (s *Session) ResetCSRF() error {
+	s.CSRFToken = Random(256)
+	_, err := sqlSessionSetCSRF.Exec(sql.Named("csrf_token", s.CSRFToken), sql.Named("id", s.ID))
 	return err
 }
--- app/session_test.go
+++ app/session_test.go
@@ -3,6 +3,7 @@
 package app_test
 
 import (
+	"database/sql"
 	"testing"
 	"time"
 
@@ -17,16 +18,17 @@ func TestSession(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Error emptying sessions table before running tests: %s", err)
 		}
+
 	}
 
 	u, err := app.UserNew("testusername", "", "", "ODSjflaksjdfhiasfd323")
-	if err == nil {
-		t.Fatalf("No error adding user for session testing")
+	if err != nil {
+		t.Fatalf("Error adding user for session testing")
 	}
 
 	t.Run("New", func(t *testing.T) {
 		reset()
-		expires := time.Now().AddDate(0, 0, 10).Round(time.Second)
+		expires := time.Time{}
 		ipAddress := "127.0.0.1"
 		userAgent := "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0"
 		s, err := app.SessionNew(u, expires, ipAddress, userAgent)
@@ -37,9 +39,84 @@ func TestSession(t *testing.T) {
 		if s.UserID != u.ID {
 			t.Fatalf("Invalid userID in session. Expected %s, got %s", u.ID, s.UserID)
 		}
-		if !s.Expires.Equal(expires) {
-			t.Fatalf("Invalid expires in session. Expected %s, got %s", expires, s.Expires)
+		if s.Expires.IsZero() {
+			t.Fatalf("Invalid expires in session. %s", s.Expires)
+		}
+		if s.IPAddress != ipAddress {
+			t.Fatalf("Invalid IP address. Expected %s, got %s", ipAddress, s.IPAddress)
+		}
+
+		if s.UserAgent != userAgent {
+			t.Fatalf("Invalid user agent. Expected %s got %s", userAgent, s.UserAgent)
+		}
+	})
+
+	t.Run("Logout", func(t *testing.T) {
+		reset()
+		s, err := app.SessionNew(u, time.Time{}, "127.0.0.1", "")
+		if err != nil {
+			t.Fatalf("Error adding new session: %s", err)
+		}
+
+		err = s.Logout()
+		if err != nil {
+			t.Fatalf("Error logging out session: %s", err)
+		}
+
+		valid := true
+		err = data.NewQuery(`select valid from sessions where id = {{arg "id"}}`).QueryRow(sql.Named("id", s.ID)).
+			Scan(&valid)
+		if err != nil {
+			t.Fatalf("Error getting session for ID %s: %s", s.ID, err)
+		}
+
+		if valid {
+			t.Fatalf("Logged out session is still valid")
 		}
 
 	})
+
+	t.Run("User", func(t *testing.T) {
+		reset()
+		s, err := app.SessionNew(u, time.Time{}, "127.0.0.1", "")
+		if err != nil {
+			t.Fatalf("Error adding new session: %s", err)
+		}
+
+		other, err := s.User()
+		if err != nil {
+			t.Fatalf("Error getting user from session: %s", err)
+		}
+
+		if u.Username != other.Username || u.ID != other.ID {
+			t.Fatalf("User from session is not equal to created user. Expected %v, got %v", u, other)
+		}
+
+	})
+	t.Run("Reset CSRF", func(t *testing.T) {
+		reset()
+		s, err := app.SessionNew(u, time.Time{}, "127.0.0.1", "")
+		if err != nil {
+			t.Fatalf("Error adding new session: %s", err)
+		}
+
+		original := s.CSRFToken
+		err = s.ResetCSRF()
+		if err != nil {
+			t.Fatalf("Error resetting csrf token in session: %s", err)
+		}
+
+		token := ""
+		err = data.NewQuery(`select csrf_token from sessions where id = {{arg "id"}}`).QueryRow(sql.Named("id", s.ID)).
+			Scan(&token)
+		if err != nil {
+			t.Fatalf("Error getting session for ID %s: %s", s.ID, err)
+		}
+
+		if original == token || token == "" {
+			t.Fatalf("CSRF token was not updated properly: %s", token)
+		}
+
+	})
+
 }
